45d1582e8c310575cb2559f6a71d8339af847f4a
nafees nazik <nafeesnazik21@gmail.com>
2022-08-06 12:53:05 +0530

    build: add post-css config

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require("tailwindcss"),
+        require("autoprefixer"),
+        process.env.NODE_ENV === "production"
+            ? require("cssnano")({
+                preset: "default",
+            })
+            : null,
+    ].filter(Boolean),
+};
