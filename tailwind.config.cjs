017aa5e8a802b9fe8e235f37b3cbaae04dccf39e
nafees nazik <nafeesnazik21@gmail.com>
2022-08-07 22:25:22 +0530

    feat: update dark class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{astro,html,ts,tsx}'],
-    darkMode: "class",
+    darkMode: ['class', '.dark-theme'],
     theme: {
         extend: {
             fontFamily: {
