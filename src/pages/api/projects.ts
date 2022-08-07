a6c47600d057afda7504a478650f2b728de897a9
nafees nazik <nafeesnazik21@gmail.com>
2022-08-07 11:54:59 +0530

    feat: return from the cache if exist

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,7 +1,18 @@
 import type { APIContext } from "astro";
 import { prisma } from "~/utils/db";
+import { cache } from "~/utils/cache";
+import { PROJECT_CACHE_KEY } from "~/constants";
 
 export async function get({ params, request }: APIContext) {
+  if (cache.has(PROJECT_CACHE_KEY)) {
+    console.log("cache");
+    const data = await cache.get(PROJECT_CACHE_KEY);
+    return {
+      body: JSON.stringify({
+        data,
+      }),
+    };
+  }
   const data = await prisma.projectCategory.findMany({
     select: {
       id: true,
@@ -16,6 +27,7 @@ export async function get({ params, request }: APIContext) {
       },
     },
   });
+  cache.set(PROJECT_CACHE_KEY, data);
   return {
     body: JSON.stringify({
       data,
