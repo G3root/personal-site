444bb65724a2951adbeb3c435287ac6813e071a0
nafees nazik <nafeesnazik21@gmail.com>
2022-08-10 18:35:28 +0530

    feat: remove tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import solid from "@astrojs/solid-js";
 import Icons from "unplugin-icons/vite";
 
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), solid()],
-    vite: {
-        plugins: [Icons({ compiler: "solid" }),]
-    }
+  integrations: [solid()],
+  vite: {
+    plugins: [Icons({ compiler: "solid" })],
+  },
 });
