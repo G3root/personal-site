7bc38caa91a060008faee557d5cea8478c5ff0fc
nafees nazik <nafeesnazik21@gmail.com>
2022-08-13 12:44:02 +0530

    feat: add nav links to consts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,11 @@ export const PROJECT_CACHE_KEY = "project";
 export const POAP_CACHE_KEY = "poap";
 export const NFT_CACHE_KEY = "nft";
 
+export const NAV_LINKS = [
+  { to: "/", label: "Home" },
+  { to: "/guestbook", label: "Guestbook" },
+  { to: "/projects", label: "Projects" },
+  { to: "/nft-gallery", label: "NFT Gallery" },
+];
+
 export const MAXIMUM_THEME_INDEX = 7;
