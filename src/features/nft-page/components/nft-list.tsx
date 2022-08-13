abc87050957e307b87e94a21f3537c7e027acab7
nafees nazik <nafeesnazik21@gmail.com>
2022-08-13 11:56:51 +0530

    feat: replace nft list component with preact

--- src/features/nft-page/components/nft-list.tsx
+++ src/features/nft-page/components/nft-list.tsx
@@ -1,16 +1,55 @@
+import { Fragment } from "preact";
 import {
-  Accordion,
-  AccordionButton,
-  AccordionHeader,
-  AccordionItem as HeadlessAccordionItem,
-  AccordionPanel as HeadlessAccordionPanel,
-} from "solid-headless";
-import { Component, createResource, For, ParentComponent } from "solid-js";
-import ChevronRight from "~icons/heroicons-outline/chevron-right";
+  Disclosure,
+  DisclosureContent,
+  useDisclosureState,
+} from "ariakit/disclosure";
+import type { DisclosureState } from "ariakit/disclosure";
+import type { GetAllNfts } from "../api/nfts";
+import { FiChevronRight, FiExternalLink } from "react-icons/fi";
 
-import { getAllNfts } from "../api/getNfts";
+interface DisclosureButtonProps {
+  name: string;
+  image: string;
+  items: number;
+  state: DisclosureState;
+}
 
-type AccordionPanelProps = {
+const DisclosureButton = (props: DisclosureButtonProps) => {
+  const { name, image, items, state } = props;
+  return (
+    <Disclosure
+      state={state}
+      className="disclosure group relative mb-3 w-full max-w-5xl rounded-2xl p-3 bg-slate-4 focus:ring-2 "
+    >
+      <span className="relative flex w-full flex-wrap items-center space-x-2">
+        <img
+          width={45}
+          height={45}
+          alt={`${name} nft collection`}
+          src={image}
+          className="rounded-full"
+        />
+        <div className="flex-1 ">
+          <div className="flex w-full items-center justify-between">
+            <h3 className=" text-lg font-semibold ">{name}</h3>
+            <div className="flex items-center space-x-2">
+              <span>{items}</span>
+              <FiChevronRight
+                /*
+      // @ts-ignore */
+                className="chevron h-5 w-5 transition delay-150 ease-out"
+                aria-hidden
+              />
+            </div>
+          </div>
+        </div>
+      </span>
+    </Disclosure>
+  );
+};
+
+interface DisclosureContentItemsProps {
   nfts: {
     id: string;
     name: string;
@@ -18,132 +57,93 @@ type AccordionPanelProps = {
     description: string;
     openseaLink?: string;
   }[];
-};
+  state: DisclosureState;
+}
 
-const AccordionPanel: Component<AccordionPanelProps> = (props) => {
+const DisclosureContentItems = (props: DisclosureContentItemsProps) => {
+  const { nfts, state } = props;
+
   return (
-    <HeadlessAccordionPanel class="mb-2">
-      <For each={props.nfts}>
-        {(item, index) => (
-          <div>
-            <div class="md:col-span-2">
+    <DisclosureContent state={state} className="mb-2">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-12">
+        {nfts.map((data) => (
+          <Fragment key={data.id}>
+            <div className="md:col-span-2">
               <img
                 width={200}
                 height={200}
-                class="rounded-md"
-                alt={`${item.name} nft`}
-                src={item.image}
+                className="rounded-md"
+                alt={`${data.name} nft`}
+                src={data.image}
               />
             </div>
-            <div class="md:col-span-10">
-              <div class="flex items-center space-x-2">
-                <h3 class="mt-4 text-lg font-semibold ">{item.name}</h3>
-                {item.openseaLink ? (
+            <div className="md:col-span-10">
+              <div className="flex items-center space-x-2">
+                <h3 className="mt-4 text-lg font-semibold ">{data.name}</h3>
+                {data.openseaLink ? (
                   <a
-                    href={item.openseaLink}
+                    href={data.openseaLink}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    {/* <FiExternalLink class="mt-4" /> */}
+                    <FiExternalLink
+                      /*
+      // @ts-ignore */
+                      className="mt-4"
+                    />
                   </a>
                 ) : null}
               </div>
-              <p class="mt-4 max-w-2xl break-words  text-gray-600 dark:text-gray-400">
-                {item.description}
+              <p className="mt-4 max-w-2xl break-words  text-gray-600 dark:text-gray-400">
+                {data.description}
               </p>
             </div>
-          </div>
-        )}
-      </For>
-    </HeadlessAccordionPanel>
+          </Fragment>
+        ))}
+      </div>
+    </DisclosureContent>
   );
 };
 
-interface AccordionItemProps {
-  name?: string;
-  image?: string;
-  items?: number;
-}
+type NftListProps = { data: GetAllNfts };
 
-const AccordionItem: ParentComponent<AccordionItemProps> = (props) => {
-  return (
-    <HeadlessAccordionItem value={props.name}>
-      <AccordionHeader>
-        <AccordionButton
-          as="div"
-          class="group relative mb-3 w-full max-w-5xl rounded-2xl p-3 bg-slate-4 focus:ring-2"
-        >
-          {({ isSelected }) => (
-            <>
-              <span class="relative flex w-full flex-wrap items-center space-x-2">
-                <img
-                  width={45}
-                  height={45}
-                  alt={`${props.name} nft collection`}
-                  src={props.image}
-                  class="rounded-full"
-                />
-                <div class="flex-1 ">
-                  <div class="flex w-full items-center justify-between">
-                    <h3 class=" text-lg font-semibold ">{props.name}</h3>
-                    <div class="flex items-center space-x-2">
-                      <span>{props.items}</span>
-
-                      <ChevronRight
-                        aria-hidden
-                        class={`${
-                          isSelected() ? "transform rotate-90" : ""
-                        } h-5 w-5 transition delay-150 ease-out`}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </span>
-            </>
-          )}
-        </AccordionButton>
-      </AccordionHeader>
-      {props.children}
-    </HeadlessAccordionItem>
-  );
-};
-
-interface FAQ {
-  question: string;
-  answer: string;
-}
-
-export const NftList = () => {
-  const [nfts] = createResource(getAllNfts);
-
-  if (typeof nfts() === undefined) {
-    return;
-  }
+export const NftList = (props: NftListProps) => {
+  const { data } = props;
+  const nftDisclosure = useDisclosureState();
+  const poapDisclosure = useDisclosureState();
 
   return (
-    <>
-      <Accordion defaultValue={undefined} class="space-y-2" toggleable>
-        <For each={nfts()?.nft}>
-          {(nft) => (
-            <AccordionItem
-              name={nft.collectionName}
-              image={nft.collectionImage}
-              items={nft.assets.length}
-            >
-              <AccordionPanel nfts={nft.assets} />
-            </AccordionItem>
-          )}
-        </For>
-      </Accordion>
-      <Accordion defaultValue={undefined} class="space-y-2" toggleable>
-        <AccordionItem
-          name={nfts()?.poap.collectionName}
-          image={nfts()?.poap.collectionImage}
-          items={nfts()?.poap.assets.length}
-        >
-          <AccordionPanel nfts={nfts()?.poap.assets ?? []} />
-        </AccordionItem>
-      </Accordion>
-    </>
+    <div className="mt-6 grid  grid-cols-1">
+      <div className="flex  flex-col gap-y-2">
+        {data.nft.map((item) => (
+          /*
+      // @ts-ignore */
+          <Fragment key={item?.id}>
+            <div>
+              <DisclosureButton
+                state={nftDisclosure}
+                name={item.collectionName}
+                image={item.collectionImage}
+                items={item.assets.length}
+              />
+            </div>
+            <DisclosureContentItems state={nftDisclosure} nfts={item.assets} />
+          </Fragment>
+        ))}
+        <div>
+          <DisclosureButton
+            state={poapDisclosure}
+            name={data.poap.collectionName}
+            image={data.poap.collectionImage}
+            items={data.poap.assets.length}
+          />
+        </div>
+
+        <DisclosureContentItems
+          state={poapDisclosure}
+          nfts={data.poap.assets}
+        />
+      </div>
+    </div>
   );
 };
