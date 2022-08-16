a6fe867b694604ae055fb123cd3baeb0afb5ac23
nafees nazik <nafeesnazik21@gmail.com>
2022-08-16 13:10:22 +0530

    feat: add types for env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly GITHUB_CLIENT_ID: string;
+  readonly GITHUB_CLIENT_SECRET: string;
+  readonly SESSION_SECRET: string;
+  readonly SITE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
