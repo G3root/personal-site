c932b7ec4b7ef5f7087b536e5288003fb85e8f40
nafees nazik <nafeesnazik21@gmail.com>
2022-08-16 13:08:43 +0530

    feat: add CRUD endpoints for guestbook messages

---
+++ src/pages/api/message/index.ts
@@ -0,0 +1,71 @@
+import type { APIContext } from "astro";
+import { MAX_MESSAGE } from "~/constants";
+import { getAllMessages } from "~/features/guest-book-page/api/guestbook";
+import { generateAuthCookie, GenerateAuthToken, GetUser } from "~/utils/auth";
+import { prisma } from "~/utils/db";
+
+export async function get({ params, request }: APIContext) {
+  const data = await getAllMessages();
+
+  return new Response(JSON.stringify({ data }), { status: 200 });
+}
+
+export async function post({ params, request }: APIContext) {
+  const user = GetUser(request.headers);
+  if (!user) {
+    return new Response("unAuthorized", {
+      status: 401,
+    });
+  }
+  const body = (await request.json()) as { content?: string };
+  console.log({ user });
+
+  if (
+    !body ||
+    !body.content ||
+    typeof body.content !== "string" ||
+    body.content.length <= 0
+  ) {
+    return new Response("unAuthorized", {
+      status: 401,
+    });
+  }
+
+  if (Number(user.gm_count) === MAX_MESSAGE) {
+    return new Response("only two messages can be created by a single user", {
+      status: 401,
+    });
+  }
+
+  const [_, data] = await prisma.$transaction([
+    prisma.guestbook.create({
+      data: {
+        content: body.content,
+        name: user.name,
+        userId: user.id,
+      },
+    }),
+    prisma.guestbook.findMany({
+      select: {
+        id: true,
+        userId: true,
+        content: true,
+        createdAt: true,
+        name: true,
+      },
+      orderBy: { createdAt: "desc" },
+    }),
+  ]);
+
+  const token = GenerateAuthToken({
+    ...user,
+    gm_count: Number(user.gm_count) + 1,
+  });
+
+  return new Response(JSON.stringify({ data }), {
+    status: 200,
+    headers: {
+      "Set-Cookie": generateAuthCookie(token),
+    },
+  });
+}
