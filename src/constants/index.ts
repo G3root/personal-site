a3e863bc0dbc19cf62aaabc1dec299996478ef11
nafees nazik <nafeesnazik21@gmail.com>
2022-08-16 13:11:01 +0530

    feat: add more constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,3 +10,10 @@ export const NAV_LINKS = [
 ];
 
 export const MAXIMUM_THEME_INDEX = 7;
+export const MAX_MESSAGE = 2;
+export const GITHUB_CLIENT_ID = import.meta.env.GITHUB_CLIENT_ID;
+export const GITHUB_CLIENT_SECRET = import.meta.env.GITHUB_CLIENT_SECRET;
+export const SESSION_SECRET = import.meta.env.SESSION_SECRET;
+export const AUTH_COOKIE_NAME = "__session_nfz";
+export const SITE_URL = import.meta.env.SITE_URL;
+export const GITHUB_AUTH_REDIRECT_URL = `${SITE_URL}/api/auth/callback/github`;
