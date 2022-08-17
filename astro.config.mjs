90e11b1c34a88154620adb1f3264106e7df4a5ee
nafees nazik <nafeesnazik21@gmail.com>
2022-08-17 23:40:16 +0530

    build: add preact as alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,5 +16,13 @@ export default defineConfig({
       external: ["svgo"],
       noExternal: ["ariakit", "ariakit-utils", "react-icons"]
     },
+    resolve: {
+      alias: {
+        "react": "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+        "react/jsx-runtime": "preact/jsx-runtime"
+      },
+    }
   }
 });
