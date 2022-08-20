a82ca01a2c220f8fa50813b9bde6e429888625b0
nafees nazik <nafeesnazik21@gmail.com>
2022-08-20 13:43:20 +0530

    style: format all files with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
 import { defineConfig } from "astro/config";
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 import preact from "@astrojs/preact";
 import compress from "astro-compress";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({
-    compat: true
-  }), compress(), image()],
-  output: 'server',
+  integrations: [
+    preact({
+      compat: true,
+    }),
+    compress(),
+    image(),
+  ],
+  output: "server",
   adapter: node(),
   vite: {
     ssr: {
       external: ["svgo"],
-      noExternal: ["ariakit", "ariakit-utils", "react-icons"]
-    }
-  }
+      noExternal: ["ariakit", "ariakit-utils", "react-icons"],
+    },
+  },
 });
