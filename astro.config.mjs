8d921f64dea3689e710abb10b36bdcfbee6c86f2
nafees nazik <nafeesnazik21@gmail.com>
2022-08-22 16:57:18 +0530

    feat: remove preact integration and add solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,19 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import preact from "@astrojs/preact";
 import compress from "astro-compress";
 import image from "@astrojs/image";
-import vitePreact from "@preact/preset-vite";
 import prefetch from "@astrojs/prefetch";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact({
-      compat: true,
-    }),
-    compress(),
-    image(),
-    prefetch(),
-  ],
+  integrations: [solidJs(), compress(), image(), prefetch()],
   output: "server",
   adapter: node(),
   vite: {
-    plugins: [vitePreact()],
     ssr: {
       external: ["svgo"],
-      noExternal: ["ariakit", "ariakit-utils", "react-icons"],
+      noExternal: ["solid-bottomsheet", "solid-headless", "solid-use"],
     },
   },
 });
