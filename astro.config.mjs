ddb989ddbbdd1eafb6068b61428319baa86f0056
nafees nazik <nafeesnazik21@gmail.com>
2023-09-12 07:32:50 +0530

    chore: remove astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import compress from "astro-compress";
-import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), compress(), image(), prefetch()],
+  integrations: [solidJs(), compress(), prefetch()],
   output: "server",
-  adapter: node(),
+  adapter: node({ mode: "standalone" }),
   vite: {
     ssr: {
       external: ["svgo"],
