081623326db8d29527c684b1c5b754c84bc77087
nafees nazik <nafeesnazik21@gmail.com>
2023-12-11 19:59:49 +0530

    fix: theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,48 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
+const { createPlugin } = require("windy-radix-palette");
+const {
+  slate,
+  slateDark,
+  blackA,
+  whiteA,
+  blue,
+  blueDark,
+  yellow,
+  yellowDark,
+  red,
+  redDark,
+  green,
+  greenDark,
+  orange,
+  orangeDark,
+  pink,
+  pinkDark,
+  violet,
+  violetDark,
+} = require("@radix-ui/colors");
+
+const colors = createPlugin({
+  slate,
+  slateDark,
+  blackA,
+  whiteA,
+  blue,
+  blueDark,
+  yellow,
+  yellowDark,
+  red,
+  redDark,
+  green,
+  greenDark,
+  orange,
+  orangeDark,
+  pink,
+  pinkDark,
+  violet,
+  violetDark,
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,ts,tsx}"],
@@ -20,6 +63,6 @@ module.exports = {
     require("@tailwindcss/forms")({
       strategy: "class", // only generate classes
     }),
+    colors.plugin,
   ],
-  presets: [require("windy-radix-palette")],
 };
