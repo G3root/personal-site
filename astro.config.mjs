444939fa2f00dc2bf9fe09d1f1d1f227a7e089c6
nafees nazik <nafeesnazik21@gmail.com>
2023-12-11 17:20:45 +0530

    feat: remove solid js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
-import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), compress(), prefetch()],
+  integrations: [compress(), prefetch()],
   output: "server",
   adapter: vercel(),
-  vite: {
-    ssr: {
-      external: ["svgo"],
-      noExternal: ["solid-bottomsheet"],
-    },
-  },
 });
